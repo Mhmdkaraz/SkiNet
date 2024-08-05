a92e6fe8da53696812962b572708ebdcde83e336
Mhmdkaraz <mohamedkaraz22@gmail.com>
2024-08-05 21:44:03 +0300

    Apply migration on app start

--- API/Program.cs
+++ API/Program.cs
@@ -35,5 +35,16 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+//create database and applies migration on app start
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+
+try {
+    await context.Database.MigrateAsync();
+}catch (Exception ex) {
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
